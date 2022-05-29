b416449cd3b7b95ec6be036969abc60e7ebe369c
Mark Drobnak <mark.drobnak@gmail.com>
2022-05-29 13:48:14 -0700

    Handle multiple connections by spawning a task per connection

--- n3ds-controller-server/src/main.rs
+++ n3ds-controller-server/src/main.rs
@@ -1,5 +1,5 @@
 use tokio::io::{AsyncBufReadExt, BufReader};
-use tokio::net::TcpListener;
+use tokio::net::{TcpListener, TcpStream};
 use tokio_stream::wrappers::TcpListenerStream;
 use tokio_stream::StreamExt;
 
@@ -12,36 +12,42 @@ async fn main() {
     let mut tcp_stream = TcpListenerStream::new(tcp_listener);
 
     while let Some(connection) = tcp_stream.next().await {
-        let connection = match connection {
-            Ok(connection) => BufReader::new(connection),
+        match connection {
+            Ok(connection) => {
+                tokio::spawn(handle_connection(connection));
+            }
             Err(e) => {
                 eprintln!("New connection error: {e}");
                 continue;
             }
-        };
-        let peer_addr = match connection.get_ref().peer_addr() {
-            Ok(peer_addr) => peer_addr,
+        }
+    }
+}
+
+async fn handle_connection(tcp_stream: TcpStream) {
+    let connection = BufReader::new(tcp_stream);
+    let peer_addr = match connection.get_ref().peer_addr() {
+        Ok(peer_addr) => peer_addr,
+        Err(e) => {
+            eprintln!("Error while getting peer address: {e}");
+            return;
+        }
+    };
+
+    println!("New connection from {peer_addr}");
+
+    let mut connection_lines = connection.lines();
+    while let Some(line) = connection_lines.next_line().await.transpose() {
+        match line {
+            Ok(line) => println!("[{peer_addr}] {line}"),
             Err(e) => {
-                eprintln!("Error while getting peer address: {e}");
-                continue;
-            }
-        };
-
-        println!("New connection from {peer_addr}");
-
-        let mut connection_lines = connection.lines();
-        while let Some(line) = connection_lines.next_line().await.transpose() {
-            match line {
-                Ok(line) => println!("[{peer_addr}] {line}"),
-                Err(e) => {
-                    eprintln!("Error while reading stream: {e}");
-                    if e.kind() == std::io::ErrorKind::ConnectionReset {
-                        break;
-                    }
+                eprintln!("Error while reading stream: {e}");
+                if e.kind() == std::io::ErrorKind::ConnectionReset {
+                    break;
                 }
             }
         }
-
-        println!("Closing connection with [{peer_addr}]");
     }
+
+    println!("Closing connection with [{peer_addr}]");
 }
