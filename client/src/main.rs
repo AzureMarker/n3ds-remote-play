a06fbabf6e9d08af2c9d837d1e9184a0a9b7b3be
AzureMarker <mark.drobnak@gmail.com>
2026-01-02 22:10:48 -0500

    Move system core thread spawning to separate file

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,5 +1,7 @@
+mod thread;
 mod video_stream;
 
+use crate::thread::spawn_system_core_thread;
 use crate::video_stream::FrameReassembler;
 use bincode::Options;
 use ctru::applets::swkbd;
@@ -14,10 +16,10 @@ use ctru::services::svc::HandleExt;
 use n3ds_remote_play_common::{CStick, CirclePad, InputState};
 use std::io::{Read, Write};
 use std::net::{Ipv4Addr, TcpStream, UdpSocket};
+use std::panic;
 use std::str::FromStr;
 use std::thread::sleep;
 use std::time::{Duration, Instant};
-use std::{cmp, mem, panic};
 use zune_jpeg::zune_core::bytestream::ZCursor;
 use zune_jpeg::zune_core::colorspace::ColorSpace;
 
@@ -98,8 +100,6 @@ struct RemotePlayClient<'gfx> {
     hid: Hid,
     // connection: TcpStream,
     // udp_connection: UdpSocket,
-    // frame_reassembler: rtp_receiver::FrameReassembler,
-    frame_reassembler: Box<dyn video_stream::FrameReassembler>,
     frame_recv_start: Option<Instant>,
     last_cpp_input: CirclePadProInputResponse,
 }
@@ -128,7 +128,6 @@ impl<'gfx> RemotePlayClient<'gfx> {
             // connection,
             // udp_connection,
             // frame_reassembler: Box::new(video_stream::RtpFrameReassembler::new()),
-            frame_reassembler: Box::new(video_stream::TcpFrameReassembler::new()),
             frame_recv_start: None,
             last_cpp_input: CirclePadProInputResponse::default(),
         }
@@ -171,7 +170,7 @@ impl<'gfx> RemotePlayClient<'gfx> {
         let (input_sender, input_receiver) = std::sync::mpsc::channel();
         let (frame_sender, frame_receiver) = std::sync::mpsc::channel();
         let result = unsafe {
-            self.spawn_system_core_thread(move || {
+            spawn_system_core_thread(&mut self.apt, move || {
                 Self::run_system_thread(connection, udp_connection, input_receiver, frame_sender)
             })
         };
@@ -315,91 +314,6 @@ impl<'gfx> RemotePlayClient<'gfx> {
                     break;
                 }
             }
-        }
-    }
-
-    unsafe fn spawn_system_core_thread(
-        &mut self,
-        f: impl FnOnce() + Send + 'static,
-    ) -> std::io::Result<libc::pthread_t> {
-        self.apt
-            .set_app_cpu_time_limit(30)
-            .expect("Failed to enable system core");
-
-        // Most of this implementation has been copied and modified from std.
-
-        let stack = 2 * 1024 * 1024; // DEFAULT_MIN_STACK_SIZE
-        let main = Box::new(move || {
-            let try_result = panic::catch_unwind(panic::AssertUnwindSafe(f));
-
-            if let Err(err) = try_result {
-                log::error!("thread panicked: {err:?}");
-            }
-        });
-
-        // SAFETY: dynamic size and alignment of the Box remain the same. See below for why the
-        // lifetime change is justified.
-        let main =
-            unsafe { Box::from_raw(Box::into_raw(main) as *mut (dyn FnOnce() + Send + 'static)) };
-        let p = Box::into_raw(Box::new(main));
-        let mut native: libc::pthread_t = unsafe { mem::zeroed() };
-        let mut attr: mem::MaybeUninit<libc::pthread_attr_t> = mem::MaybeUninit::uninit();
-        assert_eq!(unsafe { libc::pthread_attr_init(attr.as_mut_ptr()) }, 0);
-
-        let stack_size = cmp::max(stack, libc::PTHREAD_STACK_MIN);
-
-        match unsafe { libc::pthread_attr_setstacksize(attr.as_mut_ptr(), stack_size) } {
-            0 => {}
-            n => {
-                assert_eq!(n, libc::EINVAL);
-                // EINVAL means |stack_size| is either too small or not a
-                // multiple of the system page size. Because it's definitely
-                // >= PTHREAD_STACK_MIN, it must be an alignment issue.
-                // Round up to the nearest page and try again.
-                // FIXME: Replaced libc::_SC_PAGESIZE with value 8 to avoid error fixed in https://github.com/rust-lang/libc/pull/4875
-                let page_size = unsafe { libc::sysconf(8) as usize };
-                let stack_size =
-                    (stack_size + page_size - 1) & (-(page_size as isize - 1) as usize - 1);
-                assert_eq!(
-                    unsafe { libc::pthread_attr_setstacksize(attr.as_mut_ptr(), stack_size) },
-                    0
-                );
-            }
-        };
-
-        // This is the 3DS specific part
-        assert_eq!(
-            unsafe { libc::pthread_attr_setprocessorid_np(attr.as_mut_ptr(), 1) },
-            0
-        );
-
-        let ret =
-            unsafe { libc::pthread_create(&mut native, attr.as_ptr(), thread_start, p as *mut _) };
-        // Note: if the thread creation fails and this assert fails, then p will
-        // be leaked. However, an alternative design could cause double-free
-        // which is clearly worse.
-        assert_eq!(unsafe { libc::pthread_attr_destroy(attr.as_mut_ptr()) }, 0);
-
-        return if ret != 0 {
-            // The thread failed to start and as a result p was not consumed. Therefore, it is
-            // safe to reconstruct the box so that it gets deallocated.
-            unsafe {
-                drop(Box::from_raw(p));
-            }
-            Err(std::io::Error::from_raw_os_error(ret))
-        } else {
-            Ok(native)
-        };
-
-        extern "C" fn thread_start(main: *mut libc::c_void) -> *mut libc::c_void {
-            unsafe {
-                // Next, set up our stack overflow handler which may get triggered if we run
-                // out of stack.
-                // let _handler = stack_overflow::Handler::new();
-                // Finally, let's run some code.
-                Box::from_raw(main as *mut Box<dyn FnOnce()>)();
-            }
-            std::ptr::null_mut()
         }
     }
 
---
+++ client/src/thread.rs
@@ -0,0 +1,86 @@
+use std::{cmp, mem, panic};
+use ctru::prelude::Apt;
+
+pub unsafe fn spawn_system_core_thread(
+    apt: &mut Apt,
+    f: impl FnOnce() + Send + 'static,
+) -> std::io::Result<libc::pthread_t> {
+    apt.set_app_cpu_time_limit(30)
+        .expect("Failed to enable system core");
+
+    // Most of this implementation has been copied and modified from std.
+
+    let stack = 2 * 1024 * 1024; // DEFAULT_MIN_STACK_SIZE
+    let main = Box::new(move || {
+        let try_result = panic::catch_unwind(panic::AssertUnwindSafe(f));
+
+        if let Err(err) = try_result {
+            log::error!("thread panicked: {err:?}");
+        }
+    });
+
+    // SAFETY: dynamic size and alignment of the Box remain the same. See below for why the
+    // lifetime change is justified.
+    let main =
+        unsafe { Box::from_raw(Box::into_raw(main) as *mut (dyn FnOnce() + Send + 'static)) };
+    let p = Box::into_raw(Box::new(main));
+    let mut native: libc::pthread_t = unsafe { mem::zeroed() };
+    let mut attr: mem::MaybeUninit<libc::pthread_attr_t> = mem::MaybeUninit::uninit();
+    assert_eq!(unsafe { libc::pthread_attr_init(attr.as_mut_ptr()) }, 0);
+
+    let stack_size = cmp::max(stack, libc::PTHREAD_STACK_MIN);
+
+    match unsafe { libc::pthread_attr_setstacksize(attr.as_mut_ptr(), stack_size) } {
+        0 => {}
+        n => {
+            assert_eq!(n, libc::EINVAL);
+            // EINVAL means |stack_size| is either too small or not a
+            // multiple of the system page size. Because it's definitely
+            // >= PTHREAD_STACK_MIN, it must be an alignment issue.
+            // Round up to the nearest page and try again.
+            // FIXME: Replaced libc::_SC_PAGESIZE with value 8 to avoid error fixed in https://github.com/rust-lang/libc/pull/4875
+            let page_size = unsafe { libc::sysconf(8) as usize };
+            let stack_size =
+                (stack_size + page_size - 1) & (-(page_size as isize - 1) as usize - 1);
+            assert_eq!(
+                unsafe { libc::pthread_attr_setstacksize(attr.as_mut_ptr(), stack_size) },
+                0
+            );
+        }
+    };
+
+    // This is the 3DS specific part
+    assert_eq!(
+        unsafe { libc::pthread_attr_setprocessorid_np(attr.as_mut_ptr(), 1) },
+        0
+    );
+
+    let ret =
+        unsafe { libc::pthread_create(&mut native, attr.as_ptr(), thread_start, p as *mut _) };
+    // Note: if the thread creation fails and this assert fails, then p will
+    // be leaked. However, an alternative design could cause double-free
+    // which is clearly worse.
+    assert_eq!(unsafe { libc::pthread_attr_destroy(attr.as_mut_ptr()) }, 0);
+
+    return if ret != 0 {
+        // The thread failed to start and as a result p was not consumed. Therefore, it is
+        // safe to reconstruct the box so that it gets deallocated.
+        unsafe {
+            drop(Box::from_raw(p));
+        }
+        Err(std::io::Error::from_raw_os_error(ret))
+    } else {
+        Ok(native)
+    };
+
+    extern "C" fn thread_start(main: *mut libc::c_void) -> *mut libc::c_void {
+        unsafe {
+            // Next, set up our stack overflow handler which may get triggered if we run
+            // out of stack.
+            // let _handler = stack_overflow::Handler::new();
+            // Finally, let's run some code.
+            Box::from_raw(main as *mut Box<dyn FnOnce()>)();
+        }
+        std::ptr::null_mut()
+    }
+}
