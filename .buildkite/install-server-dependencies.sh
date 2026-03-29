fd5eb808a5285ab0d60409d1437479823e8edf1a
Mark Drobnak <mark.drobnak@gmail.com>
2026-03-28 23:22:48 -0700

    Fix build issues

--- .buildkite/install-server-dependencies.sh
+++ .buildkite/install-server-dependencies.sh
@@ -2,6 +2,6 @@
 # Install dependencies needed to build the server (
 
 apt-get update
-apt-get install -y pkg-config libclang-dev libxcb1-dev libxrandr-dev \
+apt-get install -y pkg-config clang libclang-dev libxcb1-dev libxrandr-dev \
   libdbus-1-dev libpipewire-0.3-dev libwayland-dev libegl-dev \
-  clang libavcodec-dev libavformat-dev libavutil-dev libavfilter-dev
+  libavcodec-dev libavformat-dev libavutil-dev libavfilter-dev libavdevice-dev
