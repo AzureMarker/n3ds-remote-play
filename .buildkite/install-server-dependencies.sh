5cabb023a81f5da62ab3431e3a7d592bee1608d2
Mark Drobnak <mark.drobnak@gmail.com>
2026-03-28 23:32:04 -0700

    Fix build issues

--- .buildkite/install-server-dependencies.sh
+++ .buildkite/install-server-dependencies.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# Install dependencies needed to build the server (
+# Install dependencies needed to build the server (xcap and ffmpeg-next crate requirements)
 
 apt-get update
 apt-get install -y pkg-config clang libclang-dev libxcb1-dev libxrandr-dev \
-  libdbus-1-dev libpipewire-0.3-dev libwayland-dev libegl-dev \
+  libdbus-1-dev libpipewire-0.3-dev libwayland-dev libegl-dev libgdm-dev \
   libavcodec-dev libavformat-dev libavutil-dev libavfilter-dev libavdevice-dev
